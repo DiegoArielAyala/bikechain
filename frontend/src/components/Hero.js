98d21a0ea62c697a4aa9f97a66852d68abcac4a6
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-02-27 08:59:29 +0100

    Agrego modificadores a Bikechain.sol

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,5 +1,5 @@
 import { Contract } from "ethers";
-import { useState, useEffect, act } from "react";
+import { useState, useEffect } from "react";
 import Contracts from "../chain-info/deployments/map.json";
 import BikechainContract from "../chain-info/contracts/Bikechain.json"
 
@@ -73,7 +73,7 @@ const Hero = ({ signer }) => {
     }
 
     const createActivity = async (time, distance, avgSpeed) => {
-        if (isContractConnected() == false) return;
+        if (isContractConnected() === false) return;
         try {
             const tx = await contract.createActivity(time, distance, avgSpeed)
             await tx.wait();
@@ -84,7 +84,7 @@ const Hero = ({ signer }) => {
     }
 
     const retrieveAllActivities = async () => {
-        if (isContractConnected() == false) return;
+        if (isContractConnected() === false) return;
         
         try {
             console.log("Retrieve all activities...")
@@ -109,7 +109,7 @@ const Hero = ({ signer }) => {
 
 
     const getFunction = async (name) => {
-        if (isContractConnected() == false) return;
+        if (isContractConnected() === false) return;
         abi.forEach(item => {
             if(item.name === name){
                 console.log(item)
@@ -137,31 +137,28 @@ const Hero = ({ signer }) => {
                 <div className="divInputTime">
                     <label>Time</label>
                     <input className="inputHours" type="number" placeholder="Hours" ></input>
-                    <input className="inputMinutes" type="number" placeholder="Minutes" required></input>
-                    <input className="inputSeconds" type="number" placeholder="Seconds" required></input>
+                    <input className="inputMinutes" type="number" placeholder="Minutes" min="0" max="59" required></input>
+                    <input className="inputSeconds" type="number" placeholder="Seconds" min="0" max="59" required></input>
                 </div>
                 <div className="divInputDistance">
                     <label>Distance</label>
-                    <input className="inputDistance" type="number" step="0.01" placeholder="Distance (Km)" required></input>
+                    <input className="inputDistance" type="number" step="0.01" placeholder="Distance (Km)" min="0" required></input>
                 </div>
                 <div className="divInputAvgSpeed">
                     <label>Average Speed</label>
-                    <input className="inputAvgSpeed" type="number" step="0.1" placeholder="Average Speed (Km/h)" required></input>
+                    <input className="inputAvgSpeed" type="number" step="0.1" placeholder="Average Speed (Km/h)" min="0" required></input>
                 </div>
                 <button type="submit" onClick={(e) => {
                     e.preventDefault()
                     const hours = document.querySelector(".inputHours");
                     const minutes = document.querySelector(".inputMinutes");
                     const seconds = document.querySelector(".inputSeconds");
-                    const time = (hours.value * 3600) + (minutes.value * 60) + (seconds.value)
+                    const time = (hours.value * 3600) + (minutes.value * 60) + Number(seconds.value)
                     const distance = document.querySelector(".inputDistance");
                     const avgSpeed = document.querySelector(".inputAvgSpeed");
                     createActivity(time, distance.value * 100, avgSpeed.value * 10)
-                    hours.value = "";
-                    minutes.value = "";
-                    seconds.value = "";
-                    distance.value = "";
-                    avgSpeed.value = "";
+                    [hours.value, minutes.value, seconds.value, distance.value, avgSpeed.value] = "";
+
                     }}>Create Activity</button>
             </form>
 
@@ -172,7 +169,7 @@ const Hero = ({ signer }) => {
                         <div key={idx} className="activity">
                             <ul>
                                 <li> ID: {activity[0]} </li>
-                                <li> Time: {Math.floor(activity[1] / 3600)}:{(Math.floor((activity[1] - (Math.floor(activity[1] / 3600)) * 3600)/60)) === 0 ? "00" : (Math.floor((activity[1] - (Math.floor(activity[1] / 3600)) * 3600)/60))}:{(activity[1] % 3600) === 0 ? "00" : (activity[1] % 3600) }  </li>
+                                <li> Time: {Math.floor(activity[1] / 3600)}:{(Math.floor((activity[1] - (Math.floor(activity[1] / 3600)) * 3600)/60)) === 0 ? "00" : (Math.floor((activity[1] - (Math.floor(activity[1] / 3600)) * 3600)/60))}:{(activity[1] % 3600) === 0 ? "00" : (activity[1] % 3600 % 60) }  </li>
                                 <li> Distance: {activity[2] / 100} Km </li>
                                 <li> AvgSpeed: {activity[3] / 10} Km/h</li>
                             </ul>
