eb121624d69aee8981d24a85df4fbf1d9f7b1c58
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-02-28 08:45:45 +0100

    Correcciones en removeActivities

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,8 @@
 import { ethers } from "ethers";
 import { useState } from "react";
-// import { Layout } from "antd";
-// import Bikechain from "../chain-info/contracts/Bikechain.json" with { type: "json" };
-// import map from "../chain-info/deployments/map.json" with { type: "json" }
-
-// const Bikechain = await fetch("../chain-info/contracts/Bikechain.json").then(res => res.json());
-// const map = await fetch("../chain-info/deployments/map.json").then(res=>res.json());
 
 // Recibe provider y signer como props y actualiza su estado en App.js
 const Header = ({ provider, setProvider, signer, setSigner }) => {
-    //console.log(window.ethereum); 
-    /* const provider = new ethers.BrowserProvider(window.ethereum);
-    console.log(provider)
-    provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    console.log(signer)
-    // const BIKECHAIN_ADDRESS = map[11155111][Bikechain][-1]
-    // console.log(BIKECHAIN_ADDRESS)
-    */
 
     const [isConnecting, setIsConnecting] = useState(false);
 
@@ -42,7 +27,7 @@ const Header = ({ provider, setProvider, signer, setSigner }) => {
             setSigner(signerInstance);
 
             setInterval(async () => {
-                if (signerInstance.address != (await providerInstance.getSigner()).address){
+                if (signerInstance.address !== (await providerInstance.getSigner()).address){
                     signerInstance = await providerInstance.getSigner();
                     setSigner(await providerInstance.getSigner())
                 }                
