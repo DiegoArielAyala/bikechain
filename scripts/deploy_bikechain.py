397ba7523a430c06959f466a1f39c48dd4357fb2
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-03-01 15:01:05 +0100

    Revision de error en existingActivitiesIds

--- scripts/deploy_bikechain.py
+++ scripts/deploy_bikechain.py
@@ -60,13 +60,41 @@ def remove_activity(id):
     print(f"Activity {id} removed")
     print("")
 
+def retrieve_activities_counter():
+    print(f"retrieve_activities_counter")
+    bikechain = Bikechain[-1]
+    activitiesCounter = bikechain.retrieveActivitiesCounter()
+    print(f"Activities counter: {activitiesCounter}")
+    print("")
+
+def retrieve_existing_activities_ids_array():
+    print(f"retrieve_existing_activities_ids_array")
+    bikechain = Bikechain[-1]
+    existringIds = bikechain.retrieveExistingActivitiesIdsArray()
+    print(f"Existing Ids: {existringIds}")
+    print("")
+
+def retrieve_existing_activities_counter():
+    print(f"retrieve_existing_activities_counter")
+    bikechain = Bikechain[-1]
+    existringCounter = bikechain.retrieveExistingActivitiesCounter()
+    print(f"Existing counter: {existringCounter}")
+    print("")
 
 def main():
     deploy_bikechain()
     create_activity(3978, 45, 25)
     create_activity(4278, 335, 215)
-    create_activity(5958, 685, 125)
+    # create_activity(5958, 685, 125)
+    #retrieve_existing_activities_counter()
     #existing_activities_ids()
     #retrieve_all_activities()
+    #retrieve_existing_activities_ids_array()
+    retrieve_activities_counter()
     remove_activity(1)
-    retrieve_owner_activities()
+    retrieve_all_activities()
+    retrieve_activities_counter()
+    existing_activities_ids()
+    retrieve_existing_activities_ids_array()
+
+    #retrieve_owner_activities()
