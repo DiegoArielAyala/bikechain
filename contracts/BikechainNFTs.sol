c112a4eb8c06e626fce9d9c442a88abeaa928b9b
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-03-03 15:17:44 +0100

    Primer commit set_tokenURI

--- contracts/BikechainNFTs.sol
+++ contracts/BikechainNFTs.sol
@@ -1,23 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Bikechain.sol";
 
-contract BikechainNFTs is ERC721, Ownable {
+contract BikechainNFTs is ERC721URIStorage, Ownable {
     constructor() ERC721("BikechainNFTs", "BCNFTs") Ownable(msg.sender) {}
 
     mapping(uint => address) idToAddress;
 
     uint ntfIdsCounter;
 
-    function _createNFT(
+    function createNFT(
         address _to,
         string memory tokenURI
-    ) internal onlyOwner {
+    ) public onlyOwner {
         _safeMint(_to, ntfIdsCounter);
-        // tokenURI(ntfIdsCounter);
+        _setTokenURI(ntfIdsCounter, tokenURI);
         idToAddress[ntfIdsCounter] = _to;
         ntfIdsCounter++;
     }
---
+++ scripts/set_tokenURI.py
@@ -0,0 +1,8 @@
+NFT_METADATA = {
+    "FIRST_ACTIVITY": "https://ipfs.io/ipfs/bafybeiev6dob2iyvqph2enksp7cs2uz3qptibx5s7symrwadbtwdb24me4"
+}
+
+def set_token_uri(type):
+    token_uri  = NFT_METADATA[type]
+    return token_uri
+
