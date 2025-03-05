81a67eaba6507357218392f1832e72b861409c69
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-03-05 08:53:02 +0100

    Funcion create_metadata completada

--- scripts/create_metadata.py
+++ scripts/create_metadata.py
@@ -3,17 +3,30 @@ from scripts.set_tokenURI import NFT_IPFS_URL
 from scripts.helpful_scripts import nft_type_mapping
 from brownie import network, BikechainNFTs
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 import json
 import requests
 
+load_dotenv()
+
 nft_to_image_uri = {"FIRST_ACTIVITY": ""}
+
+pinata_api_key= os.getenv("PINATA_API_KEY")
+pinata_secret_api_key= os.getenv("PINATA_API_SECRET")
+
+headers = {
+    "pinata_api_key": pinata_api_key,
+    "pinata_secret_api_key": pinata_secret_api_key
+}
 
 
 def main():
     # Funcion que obtenga el token_id de cada nft creado, revise en la carpeta metadata si existe la metadata de ese nft, en el caso que no exista, la crea. A traves de mapping, obtiene que tipo de nft es y le asigna la metadata correspondiente
 
     metadata = metadata_template
+    metadata_dir = f"./metadata/{network.show_active()}"
+    os.makedirs(metadata_dir, exist_ok=True)
 
     bikechain = BikechainNFTs[-1]
     for token_id in range(bikechain.ntfIdsCounter()):
@@ -42,11 +55,11 @@ def main():
 def upload_to_ipfs(path):
     with Path(path).open("rb") as fp:
         image_binary = fp.read()
-        ipfs_url = "http://127.0.0.1:5001"
-        endpoint = "/api/v0/add"
-        response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
-        ipfs_hash = response.json()["Hash"]
+        # ipfs_url = "http://127.0.0.1:5001"
+        # endpoint = "/api/v0/add"
+        response = requests.post("https://api.pinata.cloud/pinning/pinFileToIPFS", files={"file": image_binary}, headers=headers)
+        ipfs_hash = response.json()["IpfsHash"]
         filename = path.split("/")[-1:][0]
         image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
-        print(image_uri)
+        print(response.json())
         return image_uri
